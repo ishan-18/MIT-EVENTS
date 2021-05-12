238804941b8b3b618284254acc792042d8348b6b
vk17-starlord <khollam.vinit@gmail.com>
2021-05-12 09:16:41 +0530

    fixed navbar

--- day3.js
+++ day3.js
@@ -122,27 +122,6 @@ function show(direction) {
 	}
 
 	newContent = `
-
-	<nav class="black z-depth-0">
-      <div class="nav-wrapper ">
-        <a href="#!" class="brand-logo">Virtual Battleground</a>
-        <a href="#"  data-target="mobile-demo" class="sidenav-trigger white-text"><i class='white-text  bx bx-menu-alt-left'></i></a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="./index.html">Home</a></li>
-          <li><a href="./ideago-index.html">Day 1</a></li>
-          <li><a href="./technica.html">Day 2</a></li>
-          <li><a href="./">Day 3</a></li>
-        </ul>
-      </div>
-      <ul class="sidenav" id="mobile-demo">
-        <h2>Virtual Battleground</h2>
-        <li><a href="./index.html">Home</a></li>
-        <li><a href="./ideago-index.html">Day 1</a></li>
-        <li><a href="./technica.html">Day 2</a></li>
-        <li><a href="./">Day 3</a></li>
-      </ul>
-    </nav>
-
   <div class="row contain">
   <span class="game-name hide-on-med-and-down" style="font-size='${fontSize}px'">${
 		content.gameName

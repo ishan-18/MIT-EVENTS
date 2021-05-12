a371601413ecdf47ea314f0d0e93bb62ceb88296
vk17-starlord <khollam.vinit@gmail.com>
2021-05-12 09:08:44 +0530

    fixed day3 title

--- day3.js
+++ day3.js
@@ -123,7 +123,7 @@ function show(direction) {
 
 	<nav class="black z-depth-0">
       <div class="nav-wrapper ">
-        <a href="#!" class="brand-logo">STRATAGEN</a>
+        <a href="#!" class="brand-logo">Virtual Battleground</a>
         <a href="#"  data-target="mobile-demo" class="sidenav-trigger white-text"><i class='white-text  bx bx-menu-alt-left'></i></a>
         <ul class="right hide-on-med-and-down">
           <li><a href="./index.html">Home</a></li>
@@ -133,7 +133,7 @@ function show(direction) {
         </ul>
       </div>
       <ul class="sidenav" id="mobile-demo">
-        <h2>TECHNICA</h2>
+        <h2>Virtual Battleground</h2>
         <li><a href="./index.html">Home</a></li>
         <li><a href="./ideago-index.html">Day 1</a></li>
         <li><a href="./technica.html">Day 2</a></li>
